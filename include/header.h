1836f4cedb61e83d749b30c665b189cfe5adb820
yinsel <yinsel@163.com>
2024-08-19 20:10:04 +0800

    update

--- include/header.h
+++ include/header.h
@@ -45,12 +45,12 @@ extern "C" INLINE _DWORD GetKernel32Addr64();
 
 extern "C" {
 #pragma function(memset)
-	void* __cdecl _memset(void* dest, int value, size_t num) {
+	void* __cdecl memset(void* dest, int value, size_t num) {
 		__stosb(static_cast<unsigned char*>(dest), static_cast<unsigned char>(value), num);
 		return dest;
 	}
 #pragma function(memcpy)
-	void* __cdecl _memcpy(void* dest, const void* src, size_t num) {
+	void* __cdecl memcpy(void* dest, const void* src, size_t num) {
 		__movsb(static_cast<unsigned char*>(dest), static_cast<const unsigned char*>(src), num);
 		return dest;
 	}
